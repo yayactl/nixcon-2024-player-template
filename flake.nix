9166443a5bc120d693e6fcc90cf93976dd144c5c
Sönke Hahn <soenkehahn@gmail.com>
2024-10-16 14:04:10 -0400

    Expose webserver as a package in the flake outputs (#4)

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
   inputs.garnix-lib = {
     url = "github:garnix-io/garnix-lib";
     inputs = {
@@ -10,16 +12,19 @@
     };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      garnix-lib,
-      ...
-    }:
+  outputs = { self, nixpkgs, garnix-lib, flake-utils }:
     let
       system = "x86_64-linux";
     in
+    (flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in rec {
+        packages = {
+          webserver = pkgs.hello;
+          default = packages.webserver;
+        };
+      }))
+    //
     {
       nixosConfigurations.server = nixpkgs.lib.nixosSystem {
         inherit system;
@@ -32,8 +37,9 @@
               githubLogin = "GITHUB_USER";
               # You only need to change this if you changed the forked repo name.
               githubRepo = "nixcon-2024-player-template";
-              # The nix derivation that will be used as the server process. It should open a webserver on port 8080.
-              webserver = pkgs.hello;
+              # The nix derivation that will be used as the server process. It
+              # should open a webserver on port 8080.
+              webserver = self.packages.${system}.webserver;
               # If you want to log in to your deployed server, put your SSH key
               # here:
               sshKey = "<YOUR_PUBLIC_SSH_KEY>";
